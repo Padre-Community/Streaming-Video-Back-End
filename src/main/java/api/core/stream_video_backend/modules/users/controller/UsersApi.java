ad9782c2ec32e02e6bf38ecfbb25735f3d6a2de6
Pedro Mendes <pedroeliasxexeu@gmail.com>
2026-07-24 01:57:41 -0300

    :rocket: add user save endpoint with password hashing

--- src/main/java/api/core/stream_video_backend/modules/users/controller/UsersApi.java
+++ src/main/java/api/core/stream_video_backend/modules/users/controller/UsersApi.java
@@ -1,6 +1,27 @@
 package api.core.stream_video_backend.modules.users.controller;
 
+import api.core.stream_video_backend.modules.users.dto.request.UsersRequest;
+import api.core.stream_video_backend.modules.users.dto.response.UsersResponse;
+import api.core.stream_video_backend.modules.users.services.UsersServices;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/users")
 public class UsersApi {
 
+    @Autowired
+    private UsersServices usersServices;
+
+    @PostMapping("/save")
+    public ResponseEntity<UsersResponse> register(@Valid @RequestBody UsersRequest request) {
+        UsersResponse response = usersServices.registerUser(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
