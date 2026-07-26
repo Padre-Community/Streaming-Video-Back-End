9587ffc505f8fcb127144ed6e2f58562a8fa5269
João Marcos <933323mk@gmail.com>
2026-07-26 19:08:46 -0300

    :refactor: correção no arquivo PlayListItemRepository

--- src/main/java/api/core/stream_video_backend/modules/videos/repository/PlayListItemRepository.java
+++ src/main/java/api/core/stream_video_backend/modules/videos/repository/PlayListItemRepository.java
@@ -2,6 +2,8 @@ package api.core.stream_video_backend.modules.videos.repository;
 
 import api.core.stream_video_backend.modules.videos.model.PlayListItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayListItemRepository extends JpaRepository<PlayListItem, Long> {
 }
