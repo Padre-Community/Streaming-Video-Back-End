36b9785e74e4dc1166d6e6d469d9f565303a985f
franciscofbmc <franciscodebrito.fbmc@gmail.com>
2026-07-28 00:08:16 +0100

    feat: PlayList

---
+++ src/main/java/api/core/stream_video_backend/modules/videos/controller/PlayListController.java
@@ -0,0 +1,15 @@
+package api.core.stream_video_backend.modules.videos.controller;
+
+import api.core.stream_video_backend.modules.videos.model.PlayList;
+import api.core.stream_video_backend.modules.videos.services.PlayListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/playlists")
+public class PlayListController {
+
+
+}
---
+++ src/main/java/api/core/stream_video_backend/modules/videos/services/PlayListService.java
@@ -0,0 +1,14 @@
+package api.core.stream_video_backend.modules.videos.services;
+
+import api.core.stream_video_backend.modules.videos.model.PlayList;
+import api.core.stream_video_backend.modules.videos.repository.PlayListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayListService {
+
+
+}
