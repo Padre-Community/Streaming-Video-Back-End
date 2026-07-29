91a40b8338740460ae0e8fe5a39c036702923e57
Igor Candal <igorcandal19@gmail.com>
2026-07-28 22:06:34 -0300

    🐛 Corrigido a classe Subscription

--- src/main/java/api/core/stream_video_backend/modules/channels/model/Subscription.java
+++ src/main/java/api/core/stream_video_backend/modules/channels/model/Subscription.java
@@ -2,9 +2,9 @@ package api.core.stream_video_backend.modules.channels.model;
 
 import api.core.stream_video_backend.modules.users.model.Users;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -25,25 +25,18 @@ public class Subscription implements Serializable {
     @Column(name = "notify_enable", nullable = false)
     private Boolean notifyEnabled;
 
+    @CreationTimestamp
     @Column(name = "subscribed_at", nullable = false)
     private LocalDateTime subscribedAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "subscriber_id", nullable = false)
+    @JsonBackReference
     private Users subscriber;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "channel_id", nullable = false)
+    @JsonBackReference
     private Channel channel;
-
-    @PrePersist
-    protected void onCreate(){
-        if (this.subscribedAt == null){
-            this.subscribedAt = LocalDateTime.now();
-        }
-        if (this.notifyEnabled == null) {
-            this.notifyEnabled = true;
-        }
-    }
 }
 
