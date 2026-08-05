7487b714893e33a7ec4ee654d5607b1199d1e249
ivancarlosantos <ivansantos.ronem@gmail.com>
2026-08-05 14:25:40 -0300

    :rocket: :bricks: adjust pipeline file

--- src/main/java/api/core/streamx/modules/audit/processor/LogProcessorInterceptor.java
+++ src/main/java/api/core/streamx/modules/audit/processor/LogProcessorInterceptor.java
@@ -2,15 +2,11 @@ package api.core.streamx.modules.audit.processor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LogProcessorInterceptor implements HandlerInterceptor {
-
-    private static final Logger log = LoggerFactory.getLogger(LogProcessorInterceptor.class);
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
