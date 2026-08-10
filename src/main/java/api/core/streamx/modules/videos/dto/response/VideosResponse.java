e654a1fb94e4518d2c277f8e5623e85cad07fc00
Higor <higorrodrigues1605@gmail.com>
2026-08-09 22:11:14 -0300

    🚀 feat: implementa listagem de vídeos por categoria

--- src/main/java/api/core/streamx/modules/videos/dto/response/VideosResponse.java
+++ src/main/java/api/core/streamx/modules/videos/dto/response/VideosResponse.java
@@ -1,4 +1,10 @@
 package api.core.streamx.modules.videos.dto.response;
 
-public record VideosResponse() {
-}
+public record VideosResponse(
+        Long id,
+        String channelName,
+        String title,
+        String thumbnailUrl,
+        Integer durationSeconds,
+        Long viewCount
+) {}
